26da9d18caf96ffe9d4b3e33131c81cbbd7165cc
ajimenezf005 <ajimenezf005@uaemex.edu.mx>
2025-11-19 23:19:20 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'pruebas_hash'
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('🟢 Conectado a la base de datos MySQL');
+  console.log('🟢 Conectado a MySQL');
 });
 
 module.exports = connection;
