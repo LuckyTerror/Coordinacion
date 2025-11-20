0da6b36a1efc6c8c2d61dbbaf9ceb99d6055150b
ajimenezf005 <ajimenezf005@uaemex.edu.mx>
2025-11-19 23:40:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 // Rutas
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/', authRoutes); // 👈 Nuevo para que /api/login funcione
-app.use('/api/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
